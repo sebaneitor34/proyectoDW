0a02fc7603739b3b96765a2277e3450f00d57d8a
sebaneitor34 <sebastian.varas@mail.udp.cl>
2024-07-07 16:47:33 -0400

    pe3

--- src/index.js
+++ src/index.js
@@ -3,14 +3,8 @@ import app from "./server.js";
 import { createAdminUser } from "./libs/createUser.js";
 import "./database.js";
 
-async function main() {
-  configDotenv();
-  await createAdminUser();
-  app.listen(app.get("port"), () => {
-    console.log("Servidor en el puerto: ", app.get("port"))
-  });
 
-  console.log("Environment:", process.env.NODE_ENV);
-}
+app.listen(app.get("port"), () => {
+  console.log("Servidor en el puerto: ", app.get("port"))
+});
 
-main();
