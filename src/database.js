c3dae7d5a5ffa15597d2d7cacebc27aecf324c27
sebaneitor34 <sebastian.varas@mail.udp.cl>
2024-07-07 16:39:17 -0400

    pe1

--- src/database.js
+++ src/database.js
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose';
-import { MONGODB_URI } from './config.js';
 
-mongoose.connect(MONGODB_URI)
-    .then(() => console.log('Database is connected'))
+const { EVENTS_APP_MONGODB_HOST, EVENTS_APP_MONGODB_DATABASE } = process.env;
+const MONGODB_URI = `mongodb://${EVENTS_APP_MONGODB_HOST}/${EVENTS_APP_MONGODB_DATABASE}`;
+
+mongoose.connect(MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+    .then(db => console.log('Database is connected'))
     .catch(err => console.log(err));
